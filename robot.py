b4bbede5d441786b651c0852d25a04205967ac9a
RootLeo00 <caterina.leonelli@gmail.com>
2023-04-20 01:49:17 +0200

    turn left and right

--- robot.py
+++ robot.py
@@ -25,3 +25,76 @@ def backward(sec):
  gpio.output(24, True)
  time.sleep(sec)
  gpio.cleanup()
+
+def forwardleft(sec):
+ init()
+ gpio.output(17, True)
+ gpio.output(27, False)
+ gpio.output(23, False) 
+ gpio.output(24, False)
+ time.sleep(sec)
+ gpio.cleanup()
+
+
+def backwardleft(sec):
+ init()
+ gpio.output(17, False)
+ gpio.output(27, True)
+ gpio.output(23, False) 
+ gpio.output(24, False)
+ time.sleep(sec)
+ gpio.cleanup()
+
+
+def forwardleft(sec):
+ init()
+ gpio.output(17, True)
+ gpio.output(27, False)
+ gpio.output(23, False) 
+ gpio.output(24, False)
+ time.sleep(sec)
+ gpio.cleanup()
+
+
+def forwardright(sec):
+ init()
+ gpio.output(17, False)
+ gpio.output(27, False)
+ gpio.output(23, True) 
+ gpio.output(24, False)
+ time.sleep(sec)
+ gpio.cleanup()
+
+
+def backwardright(sec):
+ init()
+ gpio.output(17, False)
+ gpio.output(27, False)
+ gpio.output(23, False) 
+ gpio.output(24, True)
+ time.sleep(sec)
+ gpio.cleanup()
+
+
+def stop():
+ init()
+ gpio.output(17, False)
+ gpio.output(27, False)
+ gpio.output(23, False) 
+ gpio.output(24, False)
+ time.sleep(1)
+ gpio.cleanup()
+
+##TESTING   
+# print( "forward")
+# forward(1)
+# print ("backward")
+# backward(1)
+# print("forward left")
+# forwardleft(1)
+# print("backward left")
+# backwardleft(1)
+# print("forward right")
+# forwardright(1)
+# print("backward right")
+# backwardright(1)
